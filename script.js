feff4bc12e5f5429b16d8e9c7d6d477e9c3318dc
LeBurks <techoburks@gmail.com>
2023-05-27 15:43:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let search = document.querySelector('#search-bar');
+let searchbox = document.querySelector('.search-box');
+let menubar = document.querySelector('#menu-bars');
+let mynav = document.querySelector('.navbar');
+
+
+
+search.onclick = () =>{
+  searchbox.classList.toggle('active');
+}
+
+menubar.onclick = () =>{
+  menubar.classList.toggle('fa-times')
+  navbar.classList.toggle('active')
+}
